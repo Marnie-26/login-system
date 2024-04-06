3756df92c12fab5347e05c063cec6c218e096a75
Marnie-26 <marniefrancisco2000@gmail.com>
2024-04-06 16:59:16 +0800

    V2 added in salcedo-master

---
+++ V2/storage/framework/views/d412466053bc88d6ba0e66c025183e0ed987715d.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Guest Login System - Practice</title>
+        <link rel="stylesheet" href="<?php echo e(asset('css/admin_login_style.css')); ?>">
+    </head>
+    <body>
+        <div class="background">
+            <div class="shape"></div>
+            <div class="shape"></div>
+        </div>
+        <form>
+            <h3>Admin Login</h3>
+            <label for="username">Username</label>
+            <input type="text" id="username">
+            <label for="password">Password</label>
+            <input type="password" id="password">
+            <button>Log In</button>
+        </form>
+    </body>
+</html>
+<?php /**PATH C:\Users\Dell\Downloads\login-system-local\V2\resources\views/admin_login.blade.php ENDPATH**/ ?>
